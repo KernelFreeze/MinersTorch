ef35b0b04574ac9e299e2e249d715618429431cb
Parker Hawke <hawkeboyz2@hotmail.com>
2020-05-08 12:45:40 -0400

    Update plugin to support Minecraft 1.13+
    https://www.spigotmc.org/threads/miners-torch-does-not-work-at-1-15-2-source.437245/

--- src/main/java/me/kernelfreeze/minerstorch/TorchListener.java
+++ src/main/java/me/kernelfreeze/minerstorch/TorchListener.java
@@ -1,83 +1,58 @@
 package me.kernelfreeze.minerstorch;
 
+import java.util.EnumSet;
+import java.util.Set;
+
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
+import org.bukkit.block.data.Directional;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.PlayerInventory;
-import org.bukkit.material.Torch;
 
-public class TorchListener
-  implements Listener
-{
-  private final MinersTorch plugin;
-  
-  public TorchListener(MinersTorch plugin)
-  {
-    this.plugin = plugin;
-  }
-  
-  @EventHandler
-  public void onPlayerInteract(PlayerInteractEvent event)
-  {
-    if (event.getAction() == Action.RIGHT_CLICK_BLOCK)
-    {
-      Player player = event.getPlayer();
-      switch (player.getItemInHand().getType())
-      {
-      case GOLD_PICKAXE: 
-      case IRON_PICKAXE: 
-      case STONE_PICKAXE: 
-      case WOOD_PICKAXE: 
-      case DIAMOND_PICKAXE: 
+public class TorchListener implements Listener {
+
+    // TODO: 1.16 - Support netherrite pickaxe
+    private static final Set<Material> PICKAXES = EnumSet.of(Material.WOODEN_PICKAXE, Material.STONE_PICKAXE, Material.GOLDEN_PICKAXE, Material.IRON_PICKAXE, Material.DIAMOND_PICKAXE);
+    private static final ItemStack TORCH = new ItemStack(Material.TORCH, 1); // Amount of 1 - for implementation
+
+    @EventHandler
+    public void onPlayerInteract(PlayerInteractEvent event) {
+        ItemStack item = event.getItem();
+        Block block = event.getClickedBlock();
+        BlockFace face = event.getBlockFace();
+        if (item == null || block == null || face == BlockFace.DOWN || event.getAction() != Action.RIGHT_CLICK_BLOCK || !PICKAXES.contains(item.getType())) {
+            return;
+        }
+
+        block = block.getRelative(face);
+        if (block.getType() != Material.AIR) {
+            return;
+        }
+
+        Player player = event.getPlayer();
         if (!player.hasPermission("minerstorch.place")) {
-          return;
-        }
-        if (player.hasPermission("minerstorch.unlimited"))
-        {
-          BlockFace face = event.getBlockFace();
-          Block block = event.getClickedBlock().getRelative(face);
-          if (block.getType() != Material.AIR) {
             return;
-          }
-          Torch torch = new Torch();
-          torch.setFacingDirection(face);
-          player.playSound(block.getLocation(), Sound.DIG_WOOD, 10.0F, 1.0F);
-          
-          block.setTypeIdAndData(torch.getItemTypeId(), torch.getData(), true);
         }
-        else if (player.getInventory().contains(Material.TORCH))
-        {
-          BlockFace face = event.getBlockFace();
-          Block block = event.getClickedBlock().getRelative(face);
-          if (block.getType() != Material.AIR) {
+
+        // If they have the permission, the torch removal will not be called. If they don't and the torch couldn't be removed, they had no torches
+        if (!player.hasPermission("minerstorch.unlimited") && !player.getInventory().removeItem(TORCH).isEmpty()) {
             return;
-          }
-          Torch torch = new Torch();
-          torch.setFacingDirection(face);
-          
-          PlayerInventory inventory = player.getInventory();
-          int slot = inventory.first(Material.TORCH);
-          ItemStack stack = inventory.getItem(slot);
-          if (stack.getAmount() < 1) {
-            return;
-          }
-          if (stack.getAmount() <= 1) {
-            inventory.remove(stack);
-          } else {
-            inventory.setItem(slot, new ItemStack(Material.TORCH, stack.getAmount() - 1));
-          }
-          player.playSound(block.getLocation(), Sound.DIG_WOOD, 10.0F, 1.0F);
-          block.setTypeIdAndData(torch.getItemTypeId(), torch.getData(), true);
         }
-        break;
-      }
+
+        if (face == BlockFace.UP) {
+            block.setType(Material.TORCH);
+        } else {
+            block.setBlockData(Bukkit.createBlockData(Material.WALL_TORCH, t -> ((Directional) t).setFacing(face)));
+        }
+
+        player.playSound(block.getLocation(), Sound.BLOCK_WOOD_PLACE, 1.0F, 1.0F);
     }
-  }
+
 }
